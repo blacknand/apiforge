947206a0dc20a8acf4f35a27dfa4b0de8b005f61
Nathan Blackburn <nblackburndeveloper@icloud.com>
2025-05-13 22:33:31 +0100

    config tests pass

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,13 +4,14 @@ from .test_core import EXPECTED_KEYS
 
 def test_yaml_config():
     config = ConfigParser.load_config("configs/api_config.yaml", "prod")
+    payload = {"title": "foo", "body": "bar", "userId": 1}
     assert isinstance(config, dict)
     assert config["base_url"] == "https://jsonplaceholder.typicode.com"
     assert config["auth"]["headers"]["Authorization"] == "Bearer dummy_token"
     assert len(config["endpoints"]) == 4
     assert config["endpoints"][0] == {"method": "GET", "path": "posts", "expected_status": 200, "expected_keys": EXPECTED_KEYS}
-    assert config["endpoints"][1] == {"method": "POST", "path": "posts", "expected_status": 201, "expected_keys": EXPECTED_KEYS}
-    assert config["endpoints"][2] == {"method": "PUT", "path": "posts/1", "expected_status": 200, "expected_keys": EXPECTED_KEYS}
+    assert config["endpoints"][1] == {"method": "POST", "path": "posts", "payload": payload, "expected_status": 201, "expected_keys": EXPECTED_KEYS}
+    assert config["endpoints"][2] == {"method": "PUT", "path": "posts/1", "payload": payload, "expected_status": 200, "expected_keys": EXPECTED_KEYS}
     assert config["endpoints"][3] == {"method": "DELETE", "path": "posts/1", "expected_status": 200, "expected_keys": []}
 
 def test_config():
