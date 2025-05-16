2d2093c4c4c1665d2d5b0f66df4e2245a9f06893
Nathan Blackburn <nblackburndeveloper@icloud.com>
2025-05-16 19:20:32 +0100

    fix test_expecetd_keys

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -22,3 +22,10 @@ def test_empty_keys():
     data = {"title": "foot"}
     expected_keys = []
     assert (validate_response(data, expected_keys=expected_keys)) is True
+
+def test_expected_keys():
+    data = {"title": "foot", "body": "bar"}
+    expected_keys_list = ["title", "body"]
+    expected_keys_tuple = ("title", "body")
+    assert (validate_response(data, expected_keys=expected_keys_list)) is True
+    assert (validate_response(data, expected_keys=expected_keys_tuple)) is True
