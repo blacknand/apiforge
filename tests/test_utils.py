87833172cc784e17bc8344e1c177bc143236c5ac
Nathan Blackburn <nblackburndeveloper@icloud.com>
2025-05-16 18:59:58 +0100

    fix incorrect use of assertion fix incorrect dictionary usage in testing_invalid_dict

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,26 +1,24 @@
+import pytest
 from apiforge.utils import validate_response
 from apiforge.core import APIForge
 from .test_core import EXPECTED_KEYS
 
-def api_forge():
-    return APIForge.from_config("configs/api_config.yaml")
-
 def test_valid_response():
     data = {"title": "foo", "body": "bar", "userId": 1}
     expected_keys = ["title", "body"]
-    assert (validate_response(data, expected_keys=expected_keys), True)
+    assert (validate_response(data, expected_keys=expected_keys)) is True
 
 def test_missing_keys():
     data = {"title": "foot"}
     expected_keys = ["title", "body"]
-    assert (validate_response(data, expected_keys=expected_keys), False)
+    assert (validate_response(data, expected_keys=expected_keys)) is False
 
 def testing_invalid_dict():
-    data = {"title": "foot"}
+    data = {"title"}
     expected_keys = ["title"]
-    assert (validate_response(data, expected_keys=expected_keys), False)
+    assert (validate_response(data, expected_keys=expected_keys)) is False
 
 def testing_empty_keys():
     data = {"title": "foot"}
     expected_keys = []
-    assert (validate_response(data, expected_keys=expected_keys), True)
+    assert (validate_response(data, expected_keys=expected_keys)) is True
